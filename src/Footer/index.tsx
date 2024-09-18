7d73a4783cbe70120f6e1a17dff5276b15afdae8
Md Shagil Nizami <109960926+shagil786@users.noreply.github.com>
2024-09-18 15:12:04 +0530

    added shared module component

---
+++ src/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer style={{
+            textAlign: 'center',
+            padding: '10px',
+            backgroundColor: '#f1f1f1'
+        }}>
+            <p>Shared Footer Component © 2024</p>
+        </footer>
+    )
+}
+
+export default Footer;
