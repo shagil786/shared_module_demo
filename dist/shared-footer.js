4e4c85a7d6a167a72e77f9b8701eacb928e6f4c0
Md Shagil Nizami <mdshagilnizami@Mds-MacBook-Pro.local>
2024-09-18 19:03:59 +0530

    changes

---
+++ dist/shared-footer.js
@@ -0,0 +1,13 @@
+// src/Footer/index.jsx
+import React from "react";
+var Footer = () => {
+  return /* @__PURE__ */ React.createElement("footer", { style: {
+    textAlign: "center",
+    padding: "10px",
+    backgroundColor: "#f1f1f1"
+  } }, /* @__PURE__ */ React.createElement("p", null, "Shared Footer Component \xA9 2024"));
+};
+var Footer_default = Footer;
+export {
+  Footer_default as default
+};
